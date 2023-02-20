1795fb6ee6404ca92f4d476470d69c5ca96c671c
Carlos <carlos@carlosmoshu.com>
2023-02-20 15:41:07 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,23 @@
 <script>
   import Button from "../button.svelte";
-  //on click redirect to url
+  const actions = [
+    { name: "Video Bienvenida", url: "http://localhost:80/welcome" },
+    { name: "Video Corporativo", url: "http://localhost:80/corporate" },
+    { name: "Porsche Contenido", url: "http://localhost:80/porsche" },
+    { name: "VR", url: "http://localhost:80/vr" },
+    { name: "Porsche + VR", url: "http://localhost:80/porschevr" },
+    { name: "Power ON", url: "http://localhost:80/poweron" },
+    { name: "Power OFF", url: "http://localhost:80/poweroff" },
+  ];
 </script>
 
-{#each [1, 2, 3, 4, 5, 6, 7] as n}
+{#each actions as action}
   <Button
-    title="Preset {n}"
+    title={action.name}
     onClick={() => {
       let frame__to__load = document.getElementById("frame__url__loader");
       if (frame__to__load) {
-        frame__to__load.src = `http://localhost:5173`;
+        frame__to__load.src = `http://localhost:80/${action.url}`;
       }
     }}
   />
