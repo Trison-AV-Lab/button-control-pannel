cdacea54593ec48bdf3e72975dd554bdca777142
Carlos <carlos@carlosmoshu.com>
2023-02-20 12:30:52 +0100

    first commit

---
+++ src/button.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let onClick = () => {};
+  export let title = "";
+</script>
+
+<button class="button" on:click={onClick}>
+  {title}
+</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+  import Button from "../button.svelte";
+  //on click redirect to url
+</script>
+
+{#each [1, 2, 3, 4, 5, 6, 7] as n}
+  <Button
+    title="Preset {n}"
+    onClick={() => {
+      let frame__to__load = document.getElementById("frame__url__loader");
+      if (frame__to__load) {
+        frame__to__load.src = `http://localhost:5173`;
+      }
+    }}
+  />
+{/each}
+
+<iframe id="frame__url__loader" title="" />
+
+<style>
+  @import "../styles.css";
+</style>
